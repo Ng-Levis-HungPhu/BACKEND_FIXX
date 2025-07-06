662b53057d82caa1f29cdd53ff62632aa57293f5
Ng-Levis-HungPhu <hungphunguyen.3103@gmail.com>
2025-07-07 04:21:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -113,7 +113,7 @@ document.querySelector('.overlap-3').addEventListener('click', async () => {
       return;
     }
 
-    const response = await fetch("http://127.0.0.1:5000/predict", {
+    const response = await fetch("https://backend-fixx.onrender.com/predict", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({mode, mach, aoa, ln, swept, lln})
